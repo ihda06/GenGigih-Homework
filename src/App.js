a43ac8f41d7ea662433f636db936a888a9328a79
ihda06 <ihdaanwari5@gmail.com>
2022-03-27 13:41:14 +0700

    upadate data of single-sample.js and looping albumcard component from new data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// import logo from './logo.svg';
 import './App.css';
 import Albumcard from './component/albumcard/Albumcard.js';
 import data from './single-sample';
@@ -10,30 +11,16 @@ function App() {
       <h1>Welcome to Spotipi</h1>
       
       <div className='Album-container'>
-      <Albumcard 
-        albumName={data.album.name}
-        songName={data.name}
-        url={data.album.images[0].url}
-        artistName={data.artists[0].name}
-      />
-      <Albumcard 
-        albumName={data.album.name}
-        songName={data.name}
-        url={data.album.images[0].url}
-        artistName={data.artists[0].name}
-      />
-      <Albumcard 
-        albumName={data.album.name}
-        songName={data.name}
-        url={data.album.images[0].url}
-        artistName={data.artists[0].name}
-      />
-      <Albumcard 
-        albumName={data.album.name}
-        songName={data.name}
-        url={data.album.images[0].url}
-        artistName={data.artists[0].name}
-      />
+        {
+          data.map((item) => (
+            <Albumcard 
+              albumName={item.album.name}
+              songName={item.name}
+              url={item.album.images[0].url}
+              artistName={item.artists[0].name}
+            />
+          ))
+        }
       </div>
     </div>
   );
