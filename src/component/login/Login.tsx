786c13d3ac48ce001c5bcdf69e01c9bff8c323cd
ihda06 <ihdaanwari5@gmail.com>
2022-04-23 01:12:59 +0700

    fix redirect uri issue

--- src/component/login/Login.tsx
+++ src/component/login/Login.tsx
@@ -11,7 +11,7 @@ const Login = () => {
         isLogin = true;
     }
     const CLIENT_ID = process.env.REACT_APP_VERCEL_SPOTIFY_KEY;
-    const REDIRECT_URI = "http://localhost:3000/";
+    const REDIRECT_URI = "homework-kappa.vercel.app";
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
     const RESPONSE_TYPE = "token";
     const SCOPE = "playlist-modify-private";
